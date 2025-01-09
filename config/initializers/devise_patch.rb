13fae6cd35b3ad967249e38fa8d8207709cce7a0
gabrielcroitoru97 <gabrielcroitoru@hotmail.com>
2025-01-09 00:38:24 +0000

    Try again

--- config/initializers/devise_patch.rb
+++ config/initializers/devise_patch.rb
@@ -1,4 +1,12 @@
 # https://github.com/heartcombo/devise/issues/5644
 Devise.setup do |config|
-  config.secret_key = Rails.application.secret_key_base
+  if Rails.application.respond_to?(:credentials) && Rails.application.credentials.secret_key_base.present?
+    puts "Using credentials secret_key_base"
+    config.secret_key = Rails.application.credentials.secret_key_base
+  elsif ENV["SECRET_KEY_BASE"].present?
+    puts "Using ENV SECRET_KEY_BASE"
+    config.secret_key = ENV["SECRET_KEY_BASE"]
+  else
+    raise "Devise secret_key is not set. Make sure SECRET_KEY_BASE is configured."
+  end
 end
