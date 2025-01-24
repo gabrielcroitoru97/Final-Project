945cb3f953b8517836e81a78324310388a2d911e
gabrielcroitoru97 <gabrielcroitoru@hotmail.com>
2025-01-24 03:06:17 +0000

    Fix asset pipeline issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
+  config.assets.version = '1.0'
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
